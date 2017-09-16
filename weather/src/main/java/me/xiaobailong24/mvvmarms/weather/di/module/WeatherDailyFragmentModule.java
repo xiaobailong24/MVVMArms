a24392ca661154f75799f53d8dfb787b607b387b
xiaobailong24 <xiaobailong24@gmail.com>
2017-09-16 11:16:29 +0800

    x)Dagger.Android Inject(Second way to inject Fragment)

--- weather/src/main/java/me/xiaobailong24/mvvmarms/weather/di/module/WeatherDailyFragmentModule.java
+++ weather/src/main/java/me/xiaobailong24/mvvmarms/weather/di/module/WeatherDailyFragmentModule.java
@@ -12,8 +12,14 @@ import me.xiaobailong24.mvvmarms.weather.mvvm.view.fragment.WeatherDailyFragment
  */
 @Module
 public abstract class WeatherDailyFragmentModule {
-
+    /**
+     * 第二种注入方式。当 Subcomponent 和 它的 Builder 没有其它方法或超类型时：
+     * {@link me.xiaobailong24.mvvmarms.weather.di.component.WeatherActivitySubcomponent}
+     * 可以不再需要 Subcomponent
+     * <p>
+     * 第一种{@link WeatherActivityModule}
+     */
     @FragmentScope
-    @ContributesAndroidInjector(modules = WeatherDailyModule.class)
+    @ContributesAndroidInjector(modules = WeatherDailyModule.class)//DataModule
     abstract WeatherDailyFragment contributeWeatherDailyFragment();
 }
--- weather/src/main/java/me/xiaobailong24/mvvmarms/weather/di/module/WeatherNowFragmentModule.java
+++ weather/src/main/java/me/xiaobailong24/mvvmarms/weather/di/module/WeatherNowFragmentModule.java
@@ -12,14 +12,14 @@ import me.xiaobailong24.mvvmarms.weather.mvvm.view.fragment.WeatherNowFragment;
  */
 @Module
 public abstract class WeatherNowFragmentModule {
-
-/*    @Binds
-    @IntoMap
-    @FragmentKey(WeatherNowFragment.class)
-    public abstract AndroidInjector.Factory<? extends Fragment>
-    bindMainFragmentInjectorFactory(WeatherNowFragmentSubcomponent.Builder builder);*/
-
+    /**
+     * 第二种注入方式。当 Subcomponent 和 它的 Builder 没有其它方法或超类型时：
+     * {@link me.xiaobailong24.mvvmarms.weather.di.component.WeatherActivitySubcomponent}
+     * 可以不再需要 Subcomponent
+     * <p>
+     * 第一种{@link WeatherActivityModule}
+     */
     @FragmentScope
-    @ContributesAndroidInjector(modules = WeatherNowModule.class)
+    @ContributesAndroidInjector(modules = WeatherNowModule.class)//DataModule
     abstract WeatherNowFragment contributeWeatherNowFragment();
 }
