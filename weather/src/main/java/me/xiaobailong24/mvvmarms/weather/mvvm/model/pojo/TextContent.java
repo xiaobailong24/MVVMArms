a27ce3fde1d6e3697cb1464a7f915c94a464ddc7
xiaobailong24 <xiaobailong24@gmail.com>
2017-10-23 13:43:25 +0800

    x)DataBinding POJO

--- weather/src/main/java/me/xiaobailong24/mvvmarms/weather/mvvm/model/pojo/TextContent.java
+++ weather/src/main/java/me/xiaobailong24/mvvmarms/weather/mvvm/model/pojo/TextContent.java
@@ -1,7 +1,6 @@
 package me.xiaobailong24.mvvmarms.weather.mvvm.model.pojo;
 
 import android.databinding.BaseObservable;
-import android.databinding.Bindable;
 
 /**
  * @author xiaobailong24
@@ -9,30 +8,12 @@ import android.databinding.Bindable;
  * 主要封装标题、内容项，配合 DataBinding，自动更新 UI
  */
 public class TextContent extends BaseObservable {
-    private String title;
-    private String content;
+    public final String title;
+    public final String content;
 
 
     public TextContent(String title, String content) {
         this.title = title;
-        this.content = content;
-    }
-
-    @Bindable
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Bindable
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
         this.content = content;
     }
 }
